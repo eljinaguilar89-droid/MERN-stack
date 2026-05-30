33490fe26da09636e658e7666584f5f1878896db
Eljin Aguilar <aguilar.180763@globalcity.sti.edu.ph>
2026-05-30 18:27:26 +0800

    added db

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
+const workoutRoutes = require('./routes/workouts')
 
 //express app
 const app = express();
 
 //middleware
+app.use(express.json());
+
 app.use((req,res,next) => {
     console.log(req.path, req.method);
     next()
 });
 
+//routes
+app.use('/api/workouts', workoutRoutes);
 
-app.get('/',(req,res) => {
-    res.json({ mssg: 'Welcome to the app' });
-});
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT,() => {
+            console.log('connected to db and listening for requests on port 4000');
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+    })
 
-// listen for requests
-app.listen(process.env.PORT,() => {
-    console.log('listening for requests on port 4000');
-})
+
 
